e5101d44a30a9046db0b53df8f40ec820fa177df
Cristian Paez <cpaez@siempro.gob.ar>
2025-06-24 01:45:43 -0300

    ahora

--- bot/twitter_api.py
+++ bot/twitter_api.py
@@ -1,22 +1,26 @@
-import tweepy
 import os
+import tweepy
 from dotenv import load_dotenv
 
-load_dotenv()  # carga variables desde .env si existiera
+load_dotenv()
 
 def create_client():
+    bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
     consumer_key = os.getenv("TWITTER_API_KEY")
     consumer_secret = os.getenv("TWITTER_API_SECRET")
     access_token = os.getenv("TWITTER_ACCESS_TOKEN")
     access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
-    auth = tweepy.OAuth1UserHandler(
-        consumer_key, consumer_secret,
-        access_token, access_token_secret
+    client = tweepy.Client(
+        bearer_token=bearer_token,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
     )
-    client = tweepy.API(auth)
     return client
 
-def tweet(status):
+def tweet(text):
     client = create_client()
-    client.update_status(status)
+    response = client.create_tweet(text=text)
+    print("Tweet publicado, id:", response.data['id'])
